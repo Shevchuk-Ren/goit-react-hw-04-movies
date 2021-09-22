f87f4fde9ac44b0990a355e6452220b68e0752ce
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-09-22 16:35:05 +0300

    add route to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
+import HomeView from '../HomeView/HomeView';
+import MoviesView from '../MoviesView/MoviesView';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <HomePage></HomePage>
+      <Route exact path="/">
+        <HomeView></HomeView>
+      </Route>
+      <Route exact path="/movies">
+        <MoviesView></MoviesView>
+      </Route>
     </div>
   );
 }
---
+++ src/components/MoviesView/MoviesView.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+
+export default function MoviesView(params) {
+    return (
+        <div>
+            <h1>Welcome to Hell!</h1>
+        </div>
+    )
+}
